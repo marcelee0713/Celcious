13a48d0249dba2455fa24bd1f9d92742132583c0
Marcelee Laylay <youmustbeajoke@gmail.com>
2023-06-24 22:32:18 +0800

    Add-to-Cart Database Logic

--- src/app/api/product-data/route.ts
+++ src/app/api/product-data/route.ts
@@ -1,4 +1,5 @@
 import { prisma } from "@/db/prisma";
+import { Prisma } from "@prisma/client";
 import { NextResponse } from "next/server";
 
 export type ProductGETBody = {
@@ -10,6 +11,12 @@ export type ProductGETBody = {
   product_image: string;
   product_name: string;
   stock: number;
+};
+
+export type RequestPOSTBody = {
+  product_id: string;
+  user_id: string;
+  amount: number;
 };
 
 export async function GET(request: Request) {
@@ -32,4 +39,126 @@ export async function GET(request: Request) {
   return NextResponse.json({ error: "No Response found!" }, { status: 400 });
 }
 
-export async function POST(req: Request) {}
+export async function POST(req: Request) {
+  const body: RequestPOSTBody = await req.json();
+
+  const cart = await prisma.cart.findUnique({
+    where: {
+      cart_id: body.user_id,
+    },
+  });
+
+  // If cart is empty or null. Create one and add the product
+  if (!cart) {
+    const newCart = await prisma.cart.create({
+      data: {
+        user: {
+          connect: {
+            id: body.user_id,
+          },
+        },
+      },
+    });
+    try {
+      await prisma.cart.update({
+        where: {
+          cart_id: newCart.cart_id,
+        },
+        data: {
+          items: {
+            create: {
+              quantity: body.amount,
+              product: {
+                connect: {
+                  id: body.product_id,
+                },
+              },
+            },
+          },
+        },
+      });
+    } catch (e) {
+      if (e instanceof Prisma.PrismaClientKnownRequestError) {
+        return NextResponse.json(
+          { error: `Error message: ${e.message}` },
+          { status: 400 }
+        );
+      }
+      return NextResponse.json(
+        { error: "Failed adding to cart!" },
+        { status: 500 }
+      );
+    }
+  } else {
+    console.log("Cart ID: " + cart.id);
+    console.log("PRODUCT ID: " + body.product_id);
+    try {
+      const alreadyExist = await prisma.cartItem.findFirst({
+        where: {
+          cartId: {
+            equals: cart.id,
+          },
+          AND: {
+            productId: {
+              equals: body.product_id,
+            },
+          },
+        },
+      });
+
+      console.log("CartItem ID: " + alreadyExist?.id);
+
+      if (alreadyExist) {
+        await prisma.cart.update({
+          where: {
+            id: cart.id,
+          },
+          data: {
+            items: {
+              update: {
+                where: {
+                  id: alreadyExist.id,
+                },
+                data: {
+                  quantity: body.amount,
+                },
+              },
+            },
+          },
+        });
+      } else {
+        await prisma.cart.update({
+          where: {
+            id: cart.id,
+          },
+          data: {
+            items: {
+              create: {
+                quantity: body.amount,
+                product: {
+                  connect: {
+                    id: body.product_id,
+                  },
+                },
+              },
+            },
+          },
+        });
+      }
+
+      return NextResponse.json(JSON.stringify("Success"), { status: 200 });
+    } catch (e) {
+      if (e instanceof Prisma.PrismaClientKnownRequestError) {
+        return NextResponse.json(
+          { error: `Error message: ${e.message}` },
+          { status: 400 }
+        );
+      }
+      console.log(e);
+      return NextResponse.json(
+        { error: "Failed adding to cart!" },
+        { status: 500 }
+      );
+    }
+  }
+}
--- src/components/landpage/Discover/Product/ProductDetails.tsx
+++ src/components/landpage/Discover/Product/ProductDetails.tsx
@@ -3,6 +3,8 @@ import { Roboto } from "next/font/google";
 import { useState } from "react";
 import StarRatingComponent from "react-star-rating-component";
 import { FaCartPlus, FaHeart } from "react-icons/fa";
+import Image from "next/image";
+import { useSession } from "next-auth/react";
 
 const robotoBold = Roboto({
   subsets: ["latin"],
@@ -27,7 +29,13 @@ export const ProductDetails = ({
   product_name,
   stock,
 }: Props) => {
+  const { data: session } = useSession();
   const [amount, setAmount] = useState(stock !== 0 ? 1 : 0);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState("");
+  const [success, setSuccess] = useState(false);
+  const [status, setStatus] = useState(false);
+
   const decreaseAmount = () => {
     if (amount !== 1) {
       setAmount(amount - 1);
@@ -37,6 +45,37 @@ export const ProductDetails = ({
     if (stock !== amount) {
       setAmount(amount + 1);
     }
+  };
+
+  const handleAddToCart = async () => {
+    setLoading(true);
+    await fetch("/api/product-data", {
+      method: "POST",
+      body: JSON.stringify({
+        product_id: id,
+        user_id: session?.user.id,
+        amount: amount,
+      }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then(async (res) => {
+        if (res.status === 200) {
+          return res;
+        } else {
+          setLoading(false);
+          setError("Please try again later!");
+          throw new Error(res.statusText);
+        }
+      })
+      .then((val) => {
+        setLoading(false);
+        setSuccess(true);
+      })
+      .catch((e) => {
+        console.log(e);
+      });
   };
   return (
     <div className="flex-1 flex flex-col gap-5">
@@ -72,7 +111,16 @@ export const ProductDetails = ({
         </button>
       </div>
       <div className="flex gap-2">
-        <div className="flex px-3 py-5 bg-primary gap-2 rounded-lg shadow-lg text-secondary transition-transform hover:-translate-y-1 cursor-pointer">
+        <div
+          onClick={() => {
+            setStatus(true);
+            setError("");
+            setSuccess(false);
+            setLoading(false);
+            handleAddToCart();
+          }}
+          className="flex px-3 py-5 bg-primary gap-2 rounded-lg shadow-lg text-secondary transition-transform hover:-translate-y-1 cursor-pointer"
+        >
           <FaCartPlus size={25} />
           <div className={`${robotoBold.className}`}>Add to Cart</div>
         </div>
@@ -81,6 +129,36 @@ export const ProductDetails = ({
           <div className={`${robotoBold.className}`}>Add to WishList</div>
         </div>
       </div>
+
+      {status && (
+        <div
+          className={`flex items-center justify-center h-12 w-72 bg-secondary shadow-lg rounded-lg border ${
+            loading && "border-primary"
+          }  ${error && "border-red-500"}  ${success && "border-green-500"}`}
+        >
+          {loading && (
+            <Image
+              src={"/loading.svg"}
+              alt="Loading..."
+              width={25}
+              height={25}
+              className="animate-spin"
+            />
+          )}
+
+          {error !== "" && (
+            <div className="text-red-600">
+              Error: <strong>{error}</strong>
+            </div>
+          )}
+
+          {success && (
+            <div className="text-green-600">
+              Success: <strong>Added to Cart!</strong>
+            </div>
+          )}
+        </div>
+      )}
     </div>
   );
 };
